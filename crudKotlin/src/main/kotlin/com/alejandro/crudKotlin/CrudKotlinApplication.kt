381100feb7537662bb83acd211a3ec94d3d863ee
aleBello1 <mgranados@ahorra.io>
2025-07-22 11:01:00 -0600

    first commit

---
+++ crudKotlin/src/main/kotlin/com/alejandro/crudKotlin/CrudKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.alejandro.crudKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudKotlinApplication>(*args)
+}
